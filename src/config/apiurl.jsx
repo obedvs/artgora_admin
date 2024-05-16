e9cb1d9d3802a0838d7b0c305a06a8a13d827551
PVega17102 <is.paulvega.17102@gmail.com>
2024-05-16 04:15:48 -0600

    .

--- src/config/apiurl.jsx
+++ src/config/apiurl.jsx
@@ -2,7 +2,7 @@
 // export const apiURL = 'http://localhost:3001';
 
 // Server Testing
-// export const apiURL = 'https://artgora-api.onrender.com';
+export const apiURL = 'https://artgora-api.onrender.com';
 
 // Vercel Deployment
-export const apiURL = '/api';
+// export const apiURL = '/api';
